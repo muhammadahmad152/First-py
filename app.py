d8185e10aa53c1ec14648e98afcd63ad36017867
Muhammad Ahmad <ahmadkhan20093@gmail.com>
2026-08-05 22:59:19 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+# Python
+# Variables
+# a = 10
+# print("The {a}value is")
+# b='ali'
+# c='a'
+# d=1.3
+# e=True
+# print(b,c,d,e)
+
+#Marksheet
+# name=input("Enter the name")
+# last=input("Enter the last name")
+# print("The fullname is",name, last)
+
+# math=int(input("Enter the math marks"))
+# english=int(input("Enter the english marks"))
+# computerscience=int(input("Enter the computerscience marks"))
+# pakistanstudies=int(input("Enter the pakistanstudies marks"))
+# obtainmarks=math+english+pakistanstudies+computerscience
+# total=400
+# percentage=obtainmarks/total*100
+# print("The obtain marks is", obtainmarks)
+# print("The percentage is", percentage)
+
+# if percentage >= 90:
+#     print("The grade is A+")
+# elif percentage >= 70:
+#     print("The grade is B")
+# elif percentage >= 60:
+#     print("The grade is C")
+# else:
+#     print("Fail") 
+
+
+#loop
+#for
+# list=[1,2,3,4,5,6]
+# for rr in list:
+#     print(rr)
+
+for ss in range(10):
+    print(ss) 
