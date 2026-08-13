0c7c5a5b9b123904c678b36159415dd48837291d
Muhammad Ahmad <ahmadkhan20093@gmail.com>
2026-08-12 22:48:27 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -73,5 +73,69 @@
 # thistuple = ("apple", "banana", "cherry", "apple", "cherry")
 # print(thistuple)
 
-thisset = {"apple", "banana", "cherry"}
-print(thisset)
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+# try:
+#     # Error aane ka risk wala code
+#     x = 10 / 0
+#     print(x)
+
+# except ZeroDivisionError:
+#     # Error ko handle karne wala code
+#     print("Ghalti: Aap kisi number ko 0 se divide nahi kar sakte!")
+
+# finally:
+#     # Yeh block har haal mein chalega
+#     print("Finally block run ho gaya hai.")
+
+# a = 0
+# b = 1
+
+# print(a / b)
+
+# a = 0
+# b = 1
+
+# try:
+#     result = a / b
+#     print("Answer:", result)
+
+# except ZeroDivisionError:
+#     print("Error aa gaya!")
+
+# finally:
+#     print("Finally block run ho gaya.")
+
+# def greeting():\
+#   print("Hello world")
+
+# greeting()
+
+
+# def add():
+#     print (10+ 20)
+
+# def add(a, b):
+#     print(a + b)
+# add(10, 20)
+
+# def add(a,b):
+#     return a + b
+# result = add(10, 20)
+# print(result)
+
+# def square(x):
+#     return x * x
+# result = square(5)
+
+# print (result)
+
+# square =lambda x:x*x
+# print(square(5))
+
+def square(x):
+    return x * x
+result = square(5)
+
+print (square(5))
